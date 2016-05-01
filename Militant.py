b47191e963259a3f837cccc7e1c48b24d4aa9d99
Shayansalesi <shawnystar@ymail.com>
2016-05-01 15:35:36 -0400

    Update Militant.py

--- Militant.py
+++ Militant.py
@@ -1,6 +1,13 @@
 import pygame
+import sys
+import os
 
 pygame.init()
+DISPLAYSURF = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Militant, the Game")
+background = pygame.Surface(DISPLAYSURF.get_size()).convert()
+background.fill((255,255,255))
+screen.blit(DISPLAYSURF, (0,0))
 
 while True:
 	
