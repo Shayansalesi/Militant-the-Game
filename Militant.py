9570d177542994a7b289ec2edbcd602d2f207687
Shayansalesi <shawnystar@ymail.com>
2016-05-09 12:51:04 -0400

    Update Militant.py

--- Militant.py
+++ Militant.py
@@ -12,7 +12,6 @@ pygame.display.set_caption("Militant, the Game")
 DISPLAYSURF.blit(startingscreen,(0,0))
 pygame.display.flip()
 
-
 #Information about the different countries
 MiddleEast = {"damage":20, "mobility":15, "health":50, "defense":20, "gold":20, "scorepoints":50}
 Australia = {"damage":40, "mobility":25, "health":60, "defense":30, "gold":40, "scorepoints":60}
@@ -22,6 +21,9 @@ SouthAmerica = {"damage":80, "mobility":40, "health":80, "defense":70, "gold":90
 Europe = {"damage":90, "mobility":50, "health":90, "defense":80, "gold":95, "scorepoints":145}
 NorthAmerica = {"damage":100, "mobility":80, "health":100, "defense":80, "gold":95, "scorepoints":145}
 
+#Information about the player
+playerStat = {"damage":100, "mobility":100, "health":100, "defense":100, "gold":100, "scorepoints":0}
+
 while True:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -30,9 +32,45 @@ while True:
             DISPLAYSURF = pygame.display.set_mode(firstBootScreen.get_size())
             firstBootScreen = firstBootScreen.convert()
             DISPLAYSURF.blit(firstBootScreen,(0,0))
-            pygame.display.flip()                       
+            
+            #Ally button locations
+            ally_Button_Africa = pygame.image.load("allybutton.jpg").convert()
+            DISPLAYSURF.blit(ally_Button_Africa,(480,240))
+            ally_Button_MiddleEast = pygame.image.load("allybutton.jpg").convert()
+            DISPLAYSURF.blit(ally_Button_MiddleEast,(580,185))
+            ally_Button_Asia = pygame.image.load("allybutton.jpg").convert()
+            DISPLAYSURF.blit(ally_Button_Asia,(660,120))
+            ally_Button_Australia = pygame.image.load("allybutton.jpg").convert()
+            DISPLAYSURF.blit(ally_Button_Australia,(810,375))
+            ally_Button_SouthAmerica = pygame.image.load("allybutton.jpg").convert()
+            DISPLAYSURF.blit(ally_Button_SouthAmerica,(265,330))
+            ally_Button_NorthAmerica = pygame.image.load("allybutton.jpg").convert()
+            DISPLAYSURF.blit(ally_Button_NorthAmerica,(150,165))
+            ally_Button_Europe = pygame.image.load("allybutton.jpg").convert()
+            DISPLAYSURF.blit(ally_Button_Europe,(510,125))
+
+            #Enemy button locations
+            Enemy_Button_Africa = pygame.image.load("attackbutton.png").convert()
+            DISPLAYSURF.blit(Enemy_Button_Africa,(440,240))
+            Enemy_Button_MiddleEast = pygame.image.load("attackbutton.png").convert()
+            DISPLAYSURF.blit(Enemy_Button_MiddleEast,(540,185))
+            Enemy_Button_Asia = pygame.image.load("attackbutton.png").convert()
+            DISPLAYSURF.blit(Enemy_Button_Asia,(620,120))
+            Enemy_Button_Australia = pygame.image.load("attackbutton.png").convert()
+            DISPLAYSURF.blit(Enemy_Button_Australia,(770,375))
+            Enemy_Button_SouthAmerica = pygame.image.load("attackbutton.png").convert()
+            DISPLAYSURF.blit(Enemy_Button_SouthAmerica,(225,330))
+            Enemy_Button_NorthAmerica = pygame.image.load("attackbutton.png").convert()
+            DISPLAYSURF.blit(Enemy_Button_NorthAmerica,(110,165))
+            Enemy_Button_Europe = pygame.image.load("attackbutton.png").convert()
+            DISPLAYSURF.blit(Enemy_Button_Europe,(470,125))
+
+        #If the player chooses to exit the game
         elif event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+        #Update the game screen
+        pygame.display.flip()
         pygame.display.update()
 
